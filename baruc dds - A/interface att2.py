34c11f07dcb9a88e649d42cb0fb552fb2b1955f7
MAJEWSK1 <89658686+MAJEWSK1@users.noreply.github.com>
2021-10-20 16:38:22 -0300

    Update interface att2.py

--- baruc dds - A/interface att2.py
+++ baruc dds - A/interface att2.py
@@ -2,19 +2,17 @@ from ValidacaoEmail import ValidacaoEmail
 from ValidacaoCPF import ValidacaoCPF
 from ValidacaoCNPJ import ValidacaoCNPJ
 
+
 escolha = input('escolha o que deseja ser validado\n (A)Validação de E-mail\n (B)Validação de CPF\n (C)Validação de CNPJ\n (S)Sair\n ')
 
 if escolha == 'A':
-    from.import ValidacaoEmail
-    ValidacaoEmail.Email()
+    ValidacaoEmail()
 
 elif escolha == 'B':
-    from.import ValidacaoCPF
-    ValidacaoCPF.CPF()
+    ValidacaoCPF()
 
 elif escolha == 'C':
-    from.import ValidacaoCNPJ
-    ValidacaoCNPJ.CNPJ()
+    ValidacaoCNPJ()
         
 else:
     print('finalizando programa...')
