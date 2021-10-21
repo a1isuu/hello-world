682f8e8f1e80bc57e44d915f606a458f44b8723b
MAJEWSK1 <89658686+MAJEWSK1@users.noreply.github.com>
2021-10-21 14:14:59 -0300

    Add files via upload

---
+++ Log.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+class Log: 
+
+    def info(message):
+        datetime.now()
+
+
+# from datetime import datetime
+
+# now = datetime.now()
+
+# current_time = now.strftime("%H:%M:%S")
+# print("Current Time =", current_time)
