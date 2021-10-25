a08dee72f97661bb6019445cc1466a451f1686ab
MAJEWSK1 <89658686+MAJEWSK1@users.noreply.github.com>
2021-10-25 16:16:33 -0300

    Update ValidacaoCPF.py

--- baruc dds - A/ValidacaoCPF.py
+++ baruc dds - A/ValidacaoCPF.py
@@ -12,7 +12,7 @@ def Quantidade(cpf):
     else:
         return True
 
-def PrimeiroDigito(cpf):
+def VerificacaoDigito(cpf):
     if Quantidade(cpf):
         return False 
     else:
@@ -21,39 +21,36 @@ def PrimeiroDigito(cpf):
         resultado=0
         controlador=10
         for numeros in cpfNumerado[0:9]:
-            resultado= numeros*controlador
+            resultado= numeroscontrolador
             acumulador+= resultado
             controlador= controlador-1 
-        acumulador=acumulador*10%11
+        acumulador=acumulador10%11
         if acumulador ==10:
             acumulador=0
         if acumulador == cpfNumerado[9]:
             return True
+
+        if Quantidade(cpf):
+            return False
+        else:
+            cpfNumerado = NumerarCPF(cpf)
+            acumulador2=0 
+            resultado2=0
+            controlador2=11
+            for numeros2 in cpfNumerado[0:10]:
+                resultado2= numeros2 * controlador2 
+                acumulador2+= resultado2
+                controlador2= controlador2 - 1 
+            acumulador2=acumulador2 * 10 % 11
+        if acumulador2 ==10:
+            acumulador2=0
+        if acumulador2 == cpfNumerado[10]:
+            return True
         else:
             return False
 
-def SegundoDigito(cpf):
-    if Quantidade(cpf):
-        return False  
-    else:
-        cpfNumerado = NumerarCPF(cpf)
-        acumulador2=0 
-        resultado2=0
-        controlador2=11
-        for numeros2 in cpfNumerado[0:10]:
-            resultado2= numeros2 * controlador2 
-            acumulador2+= resultado2
-            controlador2= controlador2 - 1 
-        acumulador2=acumulador2 * 10 % 11
-    if acumulador2 ==10:
-       acumulador2=0
-    if acumulador2 == cpfNumerado[10]:
-        return True
-    else:
-        return False
-        
 def ValidateCpf(cpf):
-    if Quantidade(cpf) and PrimeiroDigito(cpf) and SegundoDigito():  
+    if Quantidade(cpf) and VerificacaoDigito(cpf):
         print('o Cpf é válido!')
     else:
         print('Cpf invalido')
