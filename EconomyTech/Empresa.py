f1172189d1241f4fc172ba3e8fa94ae6b6fcd113
a1isuu <89662896+a1isuu@users.noreply.github.com>
2022-02-09 11:27:40 -0300

    Delete EconomyTech directory

--- EconomyTech/Empresa.py
+++
@@ -1,9 +0,0 @@
-def dadosEmpresa(info):
-    print('Nome da Empresa:', info[0])
-    print('Email da Empresa:', info[1])
-    print('Endereço:\nRua: {0}\nNúmero: {1}\nBairro: {2}\nCidade: {3}\nEstado: {4}\nCep: {5}' .format(info[2], info[3], info[4], info[5], info[6], info[7]))
-    print('CNPJ:', info[8])
-    print('Telefone de Contato:', info[9])
-    print('Tipo da Empresa:', info[10])
-    print('Número de Funcionários:', info[11])
-    print('Porte da Empresa:', info[12],'\n')
